a77d5450e5a3ed6e3e3152dc387eb8b131f1d71a
unknown <ilvolkov1@gmail.com>
2022-03-27 14:37:35 +0300

    added fixture to create test user and test for log in

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 import names
 import time
 from pages.register_page import RegisterPage
+from pages.locators import BasePageLocators
 
 
 @pytest.fixture(scope="function")
@@ -12,11 +13,10 @@ def create_new_user(browser):
     last_name = names.get_last_name()
     password = "12345678"
     email = first_name + last_name + "@fakemail.org"
-
     register_page = RegisterPage(browser, link)
     register_page.open()
     register_page.register_new_user(first_name, last_name, email, password)
-    time.sleep(2)
+    register_page.get_page_element(*BasePageLocators.LOGOUT_LINK).click()
     return email, password
 
 
--- test_login_page.py
+++ test_login_page.py
@@ -1,8 +1,5 @@
 import time
 from pages.login_page import LoginPage
-from pages.register_page import RegisterPage
-import names
-import pytest
 
 
 def test_user_can_log_in(browser, create_new_user):
@@ -10,15 +7,8 @@ def test_user_can_log_in(browser, create_new_user):
     data = create_new_user
     email = data[0]
     password = data[1]
-    time.sleep(2)
-    browser.switch_to.new_window("window")
-    time.sleep(2)
     login_page = LoginPage(browser, link)
-
     login_page.open()
-    time.sleep(2)
     login_page.should_be_login_page()
     login_page.log_in(email, password)
     login_page.should_be_logged_in(email)
-    time.sleep(2)
-
