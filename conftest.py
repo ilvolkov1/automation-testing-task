3ac64f53983f592a1a48634452e9f6a49b0d97d9
unknown <ilvolkov1@gmail.com>
2022-03-27 11:57:18 +0300

    added fixture to create new user

--- conftest.py
+++ conftest.py
@@ -1,10 +1,31 @@
 import pytest
 from selenium import webdriver
+import names
+import time
+from pages.register_page import RegisterPage
+
+
+@pytest.fixture(scope="function")
+def create_new_user(browser):
+    link = "http://demowebshop.tricentis.com/register"
+    first_name = names.get_first_name()
+    last_name = names.get_last_name()
+    password = "12345678"
+    email = first_name + last_name + "@fakemail.org"
+
+    register_page = RegisterPage(browser, link)
+    register_page.open()
+    register_page.register_new_user(first_name, last_name, email, password)
+    time.sleep(2)
+    return email, password
 
 
 @pytest.fixture(scope="function")
 def browser():
-    browser = webdriver.Chrome()
+    c = webdriver.ChromeOptions()
+    c.add_argument("--incognito")
+    browser = webdriver.Chrome(options=c)
     yield browser
     browser.quit()
+
 
