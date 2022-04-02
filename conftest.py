a7fad9361df7314ecee0d5da1242c5df87e7fd50
unknown <ilvolkov1@gmail.com>
2022-04-02 15:35:20 +0300

    added option for headless tests launching

--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ import names
 import time
 from pages.register_page import RegisterPage
 from pages.locators import BasePageLocators
+from selenium.webdriver.firefox.options import Options as firefox_options
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function")
@@ -21,20 +23,28 @@ def create_new_user(browser):
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--headless', action='store', default=False,
+                     help="You can run browser in headless way by adding 'True' option")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
+    browser_option = request.config.getoption("headless")
+    options_chrome = Options()
+    options_firefox = firefox_options()
+    if browser_option:
+        options_chrome.headless = True
+        options_firefox.headless = True
+
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=options_chrome)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        browser = webdriver.Firefox(options=options_firefox)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
