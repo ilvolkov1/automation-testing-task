585131bacce9a7d7d4cc9ee15be536bfa2128733
unknown <ilvolkov1@gmail.com>
2022-04-02 14:51:17 +0300

    added option to launch test via firefox or chrome

--- conftest.py
+++ conftest.py
@@ -20,12 +20,24 @@ def create_new_user(browser):
     return email, password
 
 
-@pytest.fixture(scope="function")
-def browser():
-    c = webdriver.ChromeOptions()
-    c.add_argument("--incognito")
-    browser = webdriver.Chrome(options=c)
-    yield browser
-    browser.quit()
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
 
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
 
