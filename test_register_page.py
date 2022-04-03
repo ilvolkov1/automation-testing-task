6636c9d2a5172836c040cb5b558b48fe006d9c08
unknown <ilvolkov1@gmail.com>
2022-04-03 15:05:48 +0300

    created test for existing email registration

--- test_register_page.py
+++ test_register_page.py
@@ -1,6 +1,7 @@
 from pages.register_page import RegisterPage
 import names
 import pytest
+from testdata import TestUser
 
 
 @pytest.mark.smoke
@@ -16,3 +17,17 @@ def test_guest_can_register(browser):
     register_page.should_be_register_page()
     register_page.register_new_user(first_name, last_name, email, password)
     register_page.should_be_success_message()
+
+
+def test_guest_cannot_register_with_existing_email(browser):
+    link = RegisterPage.register_page_link
+    first_name = names.get_first_name()
+    last_name = names.get_last_name()
+    password = "12345678"
+    email = TestUser.email
+
+    register_page = RegisterPage(browser, link)
+    register_page.open()
+    register_page.should_be_register_page()
+    register_page.register_new_user(first_name, last_name, email, password)
+    register_page.should_be_message_about_email_already_exist()
