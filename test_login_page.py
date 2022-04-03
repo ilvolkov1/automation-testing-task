1bb95405339a5b8a5d0fa6f6ecc9e6f07c853c13
unknown <ilvolkov1@gmail.com>
2022-04-03 15:57:43 +0300

    created test for login page with invalid data

--- test_login_page.py
+++ test_login_page.py
@@ -14,3 +14,14 @@ def test_user_can_log_in(browser):
     login_page.should_be_login_page()
     login_page.log_in(email, password)
     login_page.should_be_logged_in(email)
+
+
+def test_user_should_see_warning_message_if_credentials_are_incorrect(browser):
+    link = LoginPage.login_page_link
+    email = TestUser.email
+    password = "!" + TestUser.password
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_page()
+    login_page.log_in(email, password)
+    login_page.should_be_warning_message_about_incorrect_credentials()
