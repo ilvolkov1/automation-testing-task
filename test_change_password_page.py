f19c63530095df99cdcf92aa9aaea7e71f6a8835
unknown <ilvolkov1@gmail.com>
2022-04-03 14:35:18 +0300

    modified change password test

--- test_change_password_page.py
+++ test_change_password_page.py
@@ -1,14 +1,14 @@
 import time
 from pages.change_password_page import ChangePasswordPage
 from pages.login_page import LoginPage
+from testdata import TestUser
 
 
-def test_user_can_change_password(browser, create_new_user):
+def test_user_can_change_password(browser):
     login_link = LoginPage.login_page_link
     change_password_link = ChangePasswordPage.change_password_page_link
-    data = create_new_user
-    email = data[0]
-    password = data[1]
+    email = TestUser.email
+    password = TestUser.password
     new_password = password*2
 
     login_page = LoginPage(browser, login_link)
@@ -20,3 +20,5 @@ def test_user_can_change_password(browser, create_new_user):
     change_password_page.should_be_change_password_page()
     change_password_page.change_password(password, new_password)
     change_password_page.should_be_success_message()
+    change_password_page.open()
+    change_password_page.change_password(new_password, password)
