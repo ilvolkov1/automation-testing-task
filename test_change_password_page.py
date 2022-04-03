21d3f99b972e0384ef4c829a6151f52fe139c27e
unknown <ilvolkov1@gmail.com>
2022-04-03 16:25:03 +0300

    created test for change password page

--- test_change_password_page.py
+++ test_change_password_page.py
@@ -24,3 +24,21 @@ def test_user_can_change_password(browser):
     change_password_page.should_be_success_message()
     change_password_page.open()
     change_password_page.change_password(new_password, password)
+
+
+def test_user_should_see_warning_message_about_wrong_old_password(browser):
+    login_link = LoginPage.login_page_link
+    change_password_link = ChangePasswordPage.change_password_page_link
+    email = TestUser.email
+    password = TestUser.password
+    new_password = "!"+password
+
+    login_page = LoginPage(browser, login_link)
+    login_page.open()
+    login_page.log_in(email, password)
+
+    change_password_page = ChangePasswordPage(browser, change_password_link)
+    change_password_page.open()
+    change_password_page.should_be_change_password_page()
+    change_password_page.change_password(new_password, new_password)
+    change_password_page.should_be_warning_message_about_wrong_old_password()
