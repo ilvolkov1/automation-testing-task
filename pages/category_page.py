b380a6270fb71e8eda103fe01451b09126c98d69
unknown <ilvolkov1@gmail.com>
2022-04-09 20:19:51 +0300

    updated category page

--- pages/category_page.py
+++ pages/category_page.py
@@ -3,6 +3,7 @@ from .locators import CategoryPageLocators
 
 
 class CategoryPage(BasePage):
+    category_page_link = "http://demowebshop.tricentis.com/books"
 
     def should_be_category_page(self):
         self.should_be_category_page_form()
@@ -12,3 +13,6 @@ class CategoryPage(BasePage):
 
     def add_to_cart(self):
         self.get_page_element(*CategoryPageLocators.ADD_TO_CART_BUTTON).click()
+
+    def should_be_success_message(self):
+        assert "The product has been added to your shopping cart" in self.get_page_element(*CategoryPageLocators.SUCCESS_MESSAGE_ADDED_TO_CART).text, "You did not added item to the cart"
